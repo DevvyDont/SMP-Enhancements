b76caaa74395cb4b7d3280eab3d99ad27c997b4f
DevvyDont <devinhall4@gmail.com>
2024-08-11 01:59:35 -0400

    smprpg: refactor anvil combination logic

--- SMPRPG/src/main/java/xyz/devvydont/smprpg/listeners/AnvilEnchantmentCombinationFixListener.java
+++ SMPRPG/src/main/java/xyz/devvydont/smprpg/listeners/AnvilEnchantmentCombinationFixListener.java
@@ -1,5 +1,6 @@
 package xyz.devvydont.smprpg.listeners;
 
+import net.kyori.adventure.text.Component;
 import org.bukkit.Material;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.event.EventHandler;
@@ -10,14 +11,14 @@ import org.bukkit.inventory.AnvilInventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.EnchantmentStorageMeta;
 import org.bukkit.inventory.meta.Repairable;
+import org.jetbrains.annotations.NotNull;
 import xyz.devvydont.smprpg.SMPRPG;
 import xyz.devvydont.smprpg.enchantments.CustomEnchantment;
 import xyz.devvydont.smprpg.enchantments.calculator.EnchantmentCalculator;
 import xyz.devvydont.smprpg.items.base.SMPItemBlueprint;
 import xyz.devvydont.smprpg.items.base.VanillaItemBlueprint;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class AnvilEnchantmentCombinationFixListener implements Listener {
 
@@ -26,6 +27,85 @@ public class AnvilEnchantmentCombinationFixListener implements Listener {
     public AnvilEnchantmentCombinationFixListener(SMPRPG plugin) {
         this.plugin = plugin;
         plugin.getServer().getPluginManager().registerEvents(this, plugin);
+    }
+
+    public record EnchantmentCombination(ItemStack result, int cost){}
+
+    /**
+     * Combines the enchantments of a 2nd item stack on to the first. Respects limits such as ItemRarity as well as
+     * returns a cost to use if this was an anvil event.
+     *
+     * @return
+     */
+    public EnchantmentCombination combineEnchantments(@NotNull ItemStack input, @NotNull ItemStack combine) {
+
+        // Retrieve the blueprints of both items
+        SMPItemBlueprint inputBlueprint = SMPRPG.getInstance().getItemService().getBlueprint(input);
+        SMPItemBlueprint combineBlueprint = SMPRPG.getInstance().getItemService().getBlueprint(combine);
+
+        // Grab the enchantments we are trying to apply to the first item
+        List<CustomEnchantment> toApply = new ArrayList<>();
+
+        Map<Enchantment, Integer> combineEnchantMap;
+        // If the item is a book, we have to retrieve the enchants differently
+        if (!combineBlueprint.isCustom() && combine.getType().equals(Material.ENCHANTED_BOOK))
+            combineEnchantMap = ((EnchantmentStorageMeta) combine.getItemMeta()).getStoredEnchants();
+        else
+            combineEnchantMap = combine.getEnchantments();
+
+        // Transform vanilla enchants to our enchant wrapper
+        for (Map.Entry<Enchantment, Integer> entry : combineEnchantMap.entrySet())
+            toApply.add(SMPRPG.getInstance().getEnchantmentService().getEnchantment(entry.getKey()).build(entry.getValue()));
+
+        // Now we actually do the enchantment application. First, we need to figure out how many new enchants
+        // this item is allowed to have so it doesn't go over its limit.
+        int newEnchantmentSlots = EnchantmentCalculator.getMaxAllowedEnchantments(inputBlueprint.getRarity(input)) - input.getEnchantments().size();
+
+        // Attempt to apply all the enchantments from the 2nd item.
+        int cost = 0;
+        ItemStack result = input.clone();
+        Collections.shuffle(toApply);
+        for (CustomEnchantment enchantment : toApply) {
+
+            int levelPresent = result.getEnchantmentLevel(enchantment.getEnchantment());
+            int levelToUse = combine.getEnchantmentLevel(enchantment.getEnchantment());
+            // Skip this enchantment if a higher level is already present.
+            if (levelPresent > levelToUse)
+                continue;
+
+            // Skip this enchantment if we don't have room for another enchantment and we don't have it.
+            if (levelPresent <= 0 && newEnchantmentSlots <= 0)
+                continue;
+
+            // Skip this enchantment if the two levels are equal and also the max level
+            if (levelPresent == levelToUse && levelToUse >= enchantment.getEnchantment().getMaxLevel())
+                continue;
+
+            // If this enchantment is new, decrement the slots we have
+            if (levelPresent <= 0)
+                newEnchantmentSlots--;
+
+            // If the enchantment levels are equal, we should use a level higher if the level can go higher
+            if (levelPresent == levelToUse && levelToUse < enchantment.getEnchantment().getMaxLevel())
+                levelToUse++;
+
+            // Apply the enchantment to the result, again depending on what item we have we have to do it differently
+            if (result.getItemMeta() instanceof EnchantmentStorageMeta meta) {
+                meta.addStoredEnchant(enchantment.getEnchantment(), levelToUse, true);
+                result.setItemMeta(meta);
+            } else
+                result.addUnsafeEnchantment(enchantment.getEnchantment(), levelToUse);
+
+            cost += enchantment.getMinimumCost().additionalPerLevelCost() * levelToUse + enchantment.getMinimumCost().baseCost();
+        }
+
+        // Now add on the repair cost from the item
+        if (result.getItemMeta() instanceof Repairable repairable)
+            cost += repairable.getRepairCost();
+
+        SMPItemBlueprint resultBlueprint = SMPRPG.getInstance().getItemService().getBlueprint(result);
+        resultBlueprint.updateMeta(result);
+        return new EnchantmentCombination(result, cost);
     }
 
     /**
@@ -45,7 +125,7 @@ public class AnvilEnchantmentCombinationFixListener implements Listener {
      * @param event
      */
     @EventHandler
-    public void onCombineEnchantmentsWithSimilarGear(PrepareAnvilEvent event) {
+    public void onAnvilCombination(PrepareAnvilEvent event) {
 
         AnvilInventory anvil = event.getInventory();
         ItemStack firstItemStack = anvil.getFirstItem();
@@ -55,186 +135,31 @@ public class AnvilEnchantmentCombinationFixListener implements Listener {
         if (firstItemStack == null || secondItemStack == null)
             return;
 
+        // We know we have items in both slots, let's set result to null to be safe and prevent unwanted behavior
+        event.setResult(null);
+
+        // We have to support books being supplied to us.
         SMPItemBlueprint firstBlueprint = plugin.getItemService().getBlueprint(firstItemStack);
         SMPItemBlueprint secondBlueprint = plugin.getItemService().getBlueprint(secondItemStack);
+        boolean inputIsBook = EnchantmentCalculator.isBook(firstBlueprint, firstItemStack);
+        boolean combineItemIsBook = EnchantmentCalculator.isBook(secondBlueprint, secondItemStack);
 
-        // If the first item is an enchanted book, we don't care about this even
-        if (firstBlueprint instanceof VanillaItemBlueprint && firstItemStack.getType().equals(Material.ENCHANTED_BOOK))
+        // If the first item is a book and the second item is a non book, we can't do anything
+        if (inputIsBook && !combineItemIsBook)
             return;
 
-        // If the second item IS an enchanted book, we don't care about this event
-        if (secondBlueprint instanceof VanillaItemBlueprint && secondItemStack.getType().equals(Material.ENCHANTED_BOOK))
+        // If both items are non books and the items are not of the same type, we can't do anything. (Trying to combine a emerald chestplate with a diamond chestplate)
+        if (!inputIsBook && !combineItemIsBook && !firstBlueprint.isItemOfType(secondItemStack))
             return;
 
-        // If both items are not the same type of item, then we cannot allow a combination to happen.
-        if (!firstBlueprint.isItemOfType(secondItemStack)) {
-            event.setResult(null);
-            anvil.setRepairCost(0);
+        // Perform the combination!!!
+        EnchantmentCombination combination = combineEnchantments(firstItemStack, secondItemStack);
+        // If the cost is 0, that means this enchantment combination got nothing done
+        if (combination.cost() <= 0)
             return;
-        }
 
-        // Now we are actually going to make modifications.
-        // We have two items in the anvil that are attempting to combine and we know that neither of them are books.
-        // And that they are the same type of item.
-        // Find out what enchantments can carry over to the new one
-        EnchantmentCalculator calculator = new EnchantmentCalculator(firstItemStack, 100, 100, 0);
-        Map<Enchantment, Integer> enchantments = new HashMap<>();
-
-        // Loop through all the enchants stored on this item and find what is valid
-        for (Map.Entry<Enchantment, Integer> entry : secondItemStack.getEnchantments().entrySet()) {
-
-            // Consider the old level
-            int level = firstItemStack.getEnchantmentLevel(entry.getKey());
-
-            // If a higher level is already present, skip this one
-            if (level > entry.getValue())
-                continue;
-
-            CustomEnchantment customEnchantment = plugin.getEnchantmentService().getEnchantment(entry.getKey());
-
-            // If the same level is already present, increment enchantment by one
-            int newLevel = entry.getValue();
-            if (level == newLevel && newLevel != customEnchantment.getMaxLevel())
-                newLevel++;
-
-            // If the enchantment is allowed, apply it
-            if (calculator.enchantmentIsAllowed(customEnchantment, enchantments.keySet()))
-                enchantments.put(entry.getKey(), newLevel);
-        }
-
-        int cost = 0;
-        Repairable repairable = (Repairable) firstItemStack.getItemMeta();
-        cost += repairable.getRepairCost();
-        repairable = (Repairable) firstItemStack.getItemMeta();
-        cost += repairable.getRepairCost();
-
-        // Set the result!
-        ItemStack result = firstItemStack.clone();
-        int maxAllowedEnchants = EnchantmentCalculator.getMaxAllowedEnchantments(firstBlueprint.getRarity(result));
-        for (Map.Entry<Enchantment, Integer> entry : enchantments.entrySet()) {
-
-            // If this item isn't allowed to have a new enchantment then skip
-            if (result.getEnchantmentLevel(entry.getKey()) > 0 && result.getEnchantments().size() >= maxAllowedEnchants)
-                continue;
-
-            // Clear the old enchantment and add the new one
-            result.removeEnchantment(entry.getKey());
-            result.addUnsafeEnchantment(entry.getKey(), entry.getValue());
-
-
-            cost += entry.getKey().getMinModifiedCost(entry.getValue());
-        }
-
-        // If we somehow didn't end up with something we can spend or we have no enchantments to apply, don't do anything
-        if (cost <= 0 || enchantments.isEmpty()) {
-            event.setResult(null);
-            anvil.setRepairCost(0);
-            return;
-        }
-
-        SMPItemBlueprint resultBlueprint = plugin.getItemService().getBlueprint(result);
-        repairable = (Repairable) result.getItemMeta();
-        repairable.setRepairCost(cost);
-        result.setItemMeta(repairable);
-        resultBlueprint.updateLore(result);
-        event.setResult(result);
-        anvil.setRepairCost(cost);
+        event.setResult(combination.result());
+        event.getInventory().setRepairCostAmount(combination.cost());
     }
 
-    /**
-     * Completely override how enchantment combination behavior works when trying to combine enchantments with items using books.
-     * If a book is allowed to go on an item, then allow it to happen
-     *
-     * @param event
-     */
-    @EventHandler
-    public void onEnchantItemWithBook(PrepareAnvilEvent event) {
-
-
-        AnvilInventory anvil = event.getInventory();
-        ItemStack firstItemStack = anvil.getFirstItem();
-        ItemStack secondItemStack = anvil.getSecondItem();
-
-        // If either item is null, we don't care about this event
-        if (firstItemStack == null || secondItemStack == null)
-            return;
-
-        SMPItemBlueprint firstBlueprint = plugin.getItemService().getBlueprint(firstItemStack);
-        SMPItemBlueprint secondBlueprint = plugin.getItemService().getBlueprint(secondItemStack);
-
-        // If the first item is an enchanted book, we don't care about this even
-        if (firstBlueprint instanceof VanillaItemBlueprint && firstItemStack.getType().equals(Material.ENCHANTED_BOOK))
-            return;
-
-        // If the second item isn't an enchanted book, we don't care about this event
-        if (!(secondBlueprint instanceof VanillaItemBlueprint) || !secondItemStack.getType().equals(Material.ENCHANTED_BOOK))
-            return;
-
-        // Now we are actually going to make modifications.
-        // We have some non-enchanted book item in the first slot, and an enchanted book in the second slot.
-        // Find out what enchantments can apply
-        EnchantmentCalculator calculator = new EnchantmentCalculator(firstItemStack, 100, 100, 0);
-        EnchantmentStorageMeta meta = (EnchantmentStorageMeta) secondItemStack.getItemMeta();
-        Map<Enchantment, Integer> enchantments = new HashMap<>();
-
-        // Loop through all the enchants stored on this book and find valid enchantments
-        for (Map.Entry<Enchantment, Integer> entry : meta.getStoredEnchants().entrySet()) {
-
-            // Consider the old level
-            int level = firstItemStack.getEnchantmentLevel(entry.getKey());
-
-            // If a higher level is already present, skip this one
-            if (level > entry.getValue())
-                continue;
-
-            CustomEnchantment customEnchantment = plugin.getEnchantmentService().getEnchantment(entry.getKey());
-
-            // If the same level is already present, increment enchantment by one
-            int newLevel = entry.getValue();
-            if (level == newLevel && newLevel != customEnchantment.getMaxLevel())
-                newLevel++;
-
-            // If the enchantment is allowed, apply it
-            if (calculator.enchantmentIsAllowed(customEnchantment, enchantments.keySet()))
-                enchantments.put(entry.getKey(), newLevel);
-        }
-
-        int cost = 0;
-        Repairable repairable = (Repairable) firstItemStack.getItemMeta();
-        cost += repairable.getRepairCost();
-        repairable = (Repairable) firstItemStack.getItemMeta();
-        cost += repairable.getRepairCost();
-
-        // Set the result!
-        ItemStack result = firstItemStack.clone();
-        int maxAllowedEnchants = EnchantmentCalculator.getMaxAllowedEnchantments(firstBlueprint.getRarity(result));
-        for (Map.Entry<Enchantment, Integer> entry : enchantments.entrySet()) {
-
-            // If this item isn't allowed to have a new enchantment then skip
-            if (result.getEnchantmentLevel(entry.getKey()) > 0 && result.getEnchantments().size() >= maxAllowedEnchants)
-                continue;
-
-            // Clear the old enchantment and add the new one
-            result.removeEnchantment(entry.getKey());
-            result.addUnsafeEnchantment(entry.getKey(), entry.getValue());
-
-
-            cost += entry.getKey().getMinModifiedCost(entry.getValue());
-        }
-
-        // If we somehow didn't end up with something we can spend or we have no enchantments to apply, don't do anything
-        if (cost <= 0 || enchantments.isEmpty()) {
-            event.setResult(null);
-            anvil.setRepairCost(0);
-            return;
-        }
-
-        SMPItemBlueprint resultBlueprint = plugin.getItemService().getBlueprint(result);
-        repairable = (Repairable) result.getItemMeta();
-        repairable.setRepairCost(cost);
-        result.setItemMeta(repairable);
-        resultBlueprint.updateLore(result);
-        event.setResult(result);
-        anvil.setRepairCost(cost);
-    }
 }
