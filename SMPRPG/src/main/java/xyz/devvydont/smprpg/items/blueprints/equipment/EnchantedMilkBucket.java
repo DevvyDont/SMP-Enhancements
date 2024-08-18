fc603a7b2eefd051239af656968fe8a6558556aa
DevvyDont <devinhall4@gmail.com>
2024-08-18 19:35:07 -0400

    smprpg/items: ench milk bucket craftable

--- SMPRPG/src/main/java/xyz/devvydont/smprpg/items/blueprints/equipment/EnchantedMilkBucket.java
+++ SMPRPG/src/main/java/xyz/devvydont/smprpg/items/blueprints/equipment/EnchantedMilkBucket.java
@@ -2,22 +2,29 @@ package xyz.devvydont.smprpg.items.blueprints.equipment;
 
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerItemConsumeEvent;
+import org.bukkit.inventory.CraftingRecipe;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.ShapedRecipe;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.inventory.recipe.CraftingBookCategory;
 import xyz.devvydont.smprpg.SMPRPG;
 import xyz.devvydont.smprpg.items.CustomItemType;
 import xyz.devvydont.smprpg.items.ItemClassification;
 import xyz.devvydont.smprpg.items.base.ChargedItemBlueprint;
+import xyz.devvydont.smprpg.items.interfaces.Craftable;
 import xyz.devvydont.smprpg.services.ActionBarService;
 import xyz.devvydont.smprpg.services.ItemService;
 import xyz.devvydont.smprpg.util.formatting.ComponentUtil;
 
+import java.util.Collection;
 import java.util.List;
 
-public class EnchantedMilkBucket extends ChargedItemBlueprint implements Listener {
+public class EnchantedMilkBucket extends ChargedItemBlueprint implements Listener, Craftable {
 
     public EnchantedMilkBucket(ItemService itemService, CustomItemType type) {
         super(itemService, type);
@@ -33,7 +40,7 @@ public class EnchantedMilkBucket extends ChargedItemBlueprint implements Listene
 
     @Override
     public int maxCharges(ItemMeta meta) {
-        return 12;
+        return 64;
     }
 
     @Override
@@ -53,5 +60,26 @@ public class EnchantedMilkBucket extends ChargedItemBlueprint implements Listene
         event.setCancelled(true);
         event.getPlayer().clearActivePotionEffects();
         SMPRPG.getInstance().getActionBarService().addActionBarComponent(event.getPlayer(), ActionBarService.ActionBarSource.MISC, ComponentUtil.getColoredComponent("CLEANSED!", NamedTextColor.GREEN), 2);
+    }
+
+    @Override
+    public NamespacedKey getRecipeKey() {
+        return new NamespacedKey(SMPRPG.getInstance(), getCustomItemType().getKey() + "-recipe");
+    }
+
+    @Override
+    public CraftingRecipe getCustomRecipe() {
+        ShapedRecipe recipe = new ShapedRecipe(getRecipeKey(), generate());
+        recipe.shape("mmm", "mmm", "mmm");
+        recipe.setIngredient('m', itemService.getCustomItem(Material.MILK_BUCKET));
+        recipe.setCategory(CraftingBookCategory.MISC);
+        return recipe;
+    }
+
+    @Override
+    public Collection<ItemStack> unlockedBy() {
+        return List.of(
+                itemService.getCustomItem(Material.BUCKET)
+        );
     }
 }
