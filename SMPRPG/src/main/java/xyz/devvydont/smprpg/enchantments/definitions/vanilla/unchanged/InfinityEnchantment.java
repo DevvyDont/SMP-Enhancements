a2c38e8f5668931922ee1d66025cd70a761a776f
DevvyDont <devinhall4@gmail.com>
2024-08-19 22:40:24 -0400

    smprpg/enchants: infinity actually works now

--- SMPRPG/src/main/java/xyz/devvydont/smprpg/enchantments/definitions/vanilla/unchanged/InfinityEnchantment.java
+++ SMPRPG/src/main/java/xyz/devvydont/smprpg/enchantments/definitions/vanilla/unchanged/InfinityEnchantment.java
@@ -9,20 +9,26 @@ import io.papermc.paper.registry.tag.TagKey;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.AbstractArrow;
+import org.bukkit.entity.Arrow;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityShootBowEvent;
 import org.bukkit.inventory.EquipmentSlotGroup;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.ItemType;
 import org.jetbrains.annotations.NotNull;
+import xyz.devvydont.smprpg.SMPRPG;
 import xyz.devvydont.smprpg.enchantments.EnchantmentUtil;
 import xyz.devvydont.smprpg.enchantments.definitions.vanilla.VanillaEnchantment;
+import xyz.devvydont.smprpg.items.base.SMPItemBlueprint;
 
 public class InfinityEnchantment extends VanillaEnchantment implements Listener {
 
     public static int getNonconsumeChance(int level) {
-        return level * 10;
+        return (level+1) * 9;
     }
 
 
@@ -72,22 +78,63 @@ public class InfinityEnchantment extends VanillaEnchantment implements Listener 
         return 20;
     }
 
-    @EventHandler
-    public void onShootBow(EntityShootBowEvent event) {
+    /*
+     * We need to make vanilla infinity not work and function as a normal bow so that the next even can work for both
+     * instances where a bow does and doesn't have infinity.
+     */
+    @EventHandler(priority = EventPriority.LOWEST)
+    public void onShotBowWithInfinity(EntityShootBowEvent event) {
+
+        // Only players can take advantage of infinity.
+        if (!(event.getEntity() instanceof Player player))
+            return;
 
         int infinityLevel = EnchantmentUtil.getHoldingEnchantLevel(getEnchantment(), event.getHand().getGroup(), event.getEntity().getEquipment());
 
+        // If we don't have infinity, then we use vanilla's logic
         if (infinityLevel <= 0)
             return;
 
-        // RNG for saving arrow
+        // RNG for saving arrow. If this check evaluates to true, we use vanilla's logic for saving an arrow with infinity.
         if (getNonconsumeChance(infinityLevel) / 100.0 > Math.random())
             return;
 
-        ItemStack consumable = event.getConsumable();
-        if (consumable == null)
+        if (event.getBow() == null)
             return;
 
-        consumable.setAmount(consumable.getAmount()-1);
+        if (!event.getBow().containsEnchantment(Enchantment.INFINITY))
+            return;
+
+        // Not sure when this should happen, but it is true even when we have infinity.
+        if (!event.shouldConsumeItem() || event.getConsumable() == null)
+            return;
+
+        SMPItemBlueprint consumableBlueprint = SMPRPG.getInstance().getItemService().getBlueprint(event.getConsumable());
+
+        // We shot a bow with infinity. Take away the consumable from the inventory.
+        for (ItemStack item : player.getInventory().getContents()) {
+
+            // No item in this inventory slot? don't care
+            if (item == null)
+                return;
+
+            // Does the consumable blueprint have a match with this item?
+            if (!consumableBlueprint.isItemOfType(item))
+                continue;
+
+            // We found an item match, decrease the amount of the item at this inventory slot by one
+            item.setAmount(item.getAmount()-1);
+
+            // If the projectile shot was an arrow, modify some attributes on it
+            // We need to allow the arrow to be picked up again, and override the itemstack to be picked up from
+            // the arrow object so that it stacks nicely back into our inventory.
+            if (event.getProjectile() instanceof Arrow arrow) {
+                arrow.setPickupStatus(AbstractArrow.PickupStatus.ALLOWED);
+                ItemStack arrowItemStack = item.clone();
+                arrowItemStack.setAmount(1);
+                arrow.setItemStack(arrowItemStack);
+            }
+            break;
+        }
     }
 }
