4ae1cb4910cbf02b72f67d9e724623eab4219fc7
DevvyDont <devinhall4@gmail.com>
2024-08-23 20:17:33 -0400

    smprpg/enchants: rebuff protection and hearty

--- SMPRPG/src/main/java/xyz/devvydont/smprpg/enchantments/definitions/HeartyEnchantment.java
+++ SMPRPG/src/main/java/xyz/devvydont/smprpg/enchantments/definitions/HeartyEnchantment.java
@@ -28,16 +28,16 @@ public class HeartyEnchantment extends CustomEnchantment implements AttributeEnc
     public static int getHealthIncrease(int level) {
         return switch (level) {
           case 0 -> 0;
-          case 1 -> 5;
-          case 2 -> 10;
-          case 3 -> 15;
-          case 4 -> 20;
-          case 5 -> 30;
-          case 6 -> 40;
-          case 7 -> 50;
-          case 8 -> 65;
-          case 9 -> 80;
-          case 10 -> 100;
+          case 1 -> 10;
+          case 2 -> 20;
+          case 3 -> 30;
+          case 4 -> 45;
+          case 5 -> 60;
+          case 6 -> 75;
+          case 7 -> 100;
+          case 8 -> 125;
+          case 9 -> 150;
+          case 10 -> 200;
           default -> getHealthIncrease(10) + 25*(level - 10);
         };
     }
