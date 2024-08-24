13301226d053af00b9b9b3c5ba267f20c846b874
DevvyDont <devinhall4@gmail.com>
2024-08-24 08:16:44 -0400

    smprpg/items: nerf strength charm

--- SMPRPG/src/main/java/xyz/devvydont/smprpg/items/blueprints/charms/StrengthCharm.java
+++ SMPRPG/src/main/java/xyz/devvydont/smprpg/items/blueprints/charms/StrengthCharm.java
@@ -4,6 +4,7 @@ import org.bukkit.inventory.EquipmentSlotGroup;
 import xyz.devvydont.smprpg.items.CustomItemType;
 import xyz.devvydont.smprpg.items.ItemClassification;
 import xyz.devvydont.smprpg.items.attribute.AttributeEntry;
+import xyz.devvydont.smprpg.items.attribute.MultiplicativeAttributeEntry;
 import xyz.devvydont.smprpg.items.attribute.ScalarAttributeEntry;
 import xyz.devvydont.smprpg.items.base.CustomFakeTotem;
 import xyz.devvydont.smprpg.services.ItemService;
@@ -21,7 +22,7 @@ public class StrengthCharm extends CustomFakeTotem {
     @Override
     public Collection<AttributeEntry> getAttributeModifiers() {
         return List.of(
-                new ScalarAttributeEntry(AttributeWrapper.STRENGTH, 1)
+                new MultiplicativeAttributeEntry(AttributeWrapper.STRENGTH, .5)
         );
     }
 
