25a1d1deb483118599f4a9e0641d4fdb605f36dd
Niall <161693828+VirtuallyNiall@users.noreply.github.com>
2024-09-03 03:04:40 +0200

    smprpg/cmd: create a simple player command wrapper
    This wrapper automatically ensures the invoker is a player.

---
+++ SMPRPG/src/main/java/xyz/devvydont/smprpg/commands/PlayerCommandBase.java
@@ -0,0 +1,24 @@
+package xyz.devvydont.smprpg.commands;
+
+import io.papermc.paper.command.brigadier.CommandSourceStack;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+import xyz.devvydont.smprpg.util.formatting.ComponentUtils;
+
+public abstract class PlayerCommandBase extends CommandBase {
+    public PlayerCommandBase(String name) {
+        super(name);
+    }
+
+    @Override
+    public void execute(@NotNull CommandSourceStack ctx, @NotNull String[] args) {
+        var sender = ctx.getSender();
+        if (sender instanceof Player player) {
+            this.playerInvoked(player, ctx, args);
+        } else {
+            sender.sendMessage(ComponentUtils.error("You are the console! You cannot do this!"));
+        }
+    }
+
+    protected abstract void playerInvoked(@NotNull Player player, @NotNull CommandSourceStack ctx, @NotNull String @NotNull [] args);
+}
