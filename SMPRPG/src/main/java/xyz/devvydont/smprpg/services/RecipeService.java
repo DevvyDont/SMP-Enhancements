0003c002d83da12a76b5b3da548be71373800887
dev <49467408+DevvyDont@users.noreply.github.com>
2025-07-12 15:59:55 -0400

    smprpg/recipe: remove debug recipe

--- SMPRPG/src/main/java/xyz/devvydont/smprpg/services/RecipeService.java
+++ SMPRPG/src/main/java/xyz/devvydont/smprpg/services/RecipeService.java
@@ -1,12 +1,6 @@
 package xyz.devvydont.smprpg.services;
 
-import org.bukkit.Bukkit;
-import org.bukkit.NamespacedKey;
-import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.BlockBreakEvent;
-import org.bukkit.inventory.ShapedRecipe;
-import xyz.devvydont.smprpg.items.CustomItemType;
 import xyz.devvydont.smprpg.listeners.crafting.CraftingTransmuteUpgradeFix;
 import xyz.devvydont.smprpg.util.listeners.ToggleableListener;
 
@@ -18,18 +12,10 @@ import java.util.List;
  */
 public class RecipeService implements IService, Listener {
 
-    public static final NamespacedKey KEY = new NamespacedKey("smprpg", "test_upgrade_recipe");
-    public static ShapedRecipe RECIPE;
-
     private final List<ToggleableListener> listeners = new ArrayList<>();
 
     @Override
     public void setup() throws RuntimeException {
-        RECIPE = new ShapedRecipe(KEY, ItemService.generate(CustomItemType.NEBULA_ROD));
-        RECIPE.shape(" a ", "ara");
-        RECIPE.setIngredient('a', ItemService.generate(CustomItemType.STOLEN_APPLE));
-        RECIPE.setIngredient('r', ItemService.generate(CustomItemType.COMET_ROD));
-        Bukkit.addRecipe(RECIPE);
 
         // Start listeners.
         listeners.add(new CraftingTransmuteUpgradeFix());
@@ -42,10 +28,4 @@ public class RecipeService implements IService, Listener {
         for (var listener : listeners)
             listener.stop();
     }
-
-    @EventHandler
-    public void onBreakDirt(BlockBreakEvent event) {
-        event.getPlayer().discoverRecipe(KEY);
-    }
-
 }
